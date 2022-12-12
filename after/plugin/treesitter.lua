e94911d756fa8820cb3f37514b0d5846fc9e20cd
chenweizi <chenweizi.chen@bytedance.com>
2022-12-12 12:09:06 +0800

    fix: 修复大文件卡顿的问题

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -6,13 +6,13 @@ ts.setup {
     enable = true,
     enable_autocmd = false,
     config = {
-      javascript = {
-        __default = '// %s',
-        jsx_element = '{/* %s */}',
-        jsx_fragment = '{/* %s */}',
-        jsx_attribute = '// %s',
-        comment = '// %s'
-      },
+      -- javascript = {
+      --   __default = '// %s',
+      --   jsx_element = '{/* %s */}',
+      --   jsx_fragment = '{/* %s */}',
+      --   jsx_attribute = '// %s',
+      --   comment = '// %s'
+      -- },
       typescript = {
         -- __default = '// %s',
         tsx_element = '{/* %s */}',
@@ -25,24 +25,24 @@ ts.setup {
   ensure_installed = {
     "tsx",
     "typescript",
-    "toml",
+    -- "toml",
     "fish",
-    "php",
+    -- "php",
     "json",
     "yaml",
-    "swift",
-    "css",
-    "html",
+    -- "swift",
+    -- "css",
+    -- "html",
     "lua"
   },
   autotag = {
-    enable = true,
+    enable = false,
   },
-    -- 启用代码高亮模块
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+  -- -- 启用代码高亮模块
+  -- highlight = {
+  --   enable = false,
+  --   additional_vim_regex_highlighting = false,
+  -- },
   -- 启用增量选择模块
   incremental_selection = {
     enable = true,
@@ -54,25 +54,38 @@ ts.setup {
     },
   },
   -- 启用代码缩进模块 (=)
+  highlight = {
+    enable = true,
+    -- disable = { "vim", "typescript", "tsx", },  
+    disable = function(lang, buf)
+        local max_filesize = 50 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+  },
   indent = {
     enable = true,
-  },
+    disable = { "vim", "typescript", "tsx", },  
+  },  
   -- p00f/nvim-ts-rainbow
-  rainbow = {
-    enable = true,
+  -- rainbow = {
+    -- enable = false,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    colors = {
-      "#95ca60",
-      "#ee6985",
-      "#D6A760",
-      "#7794f4",
-      "#b38bf5",
-      "#7cc7fe",
-    }, -- table of hex strings
+    -- disable = {"tsx"},
+    -- extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    -- max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {
+    --   "#95ca60",
+    --   "#ee6985",
+    --   "#D6A760",
+    --   "#7794f4",
+    --   "#b38bf5",
+    --   "#7cc7fe",
+    -- }, -- table of hex strings
     -- termcolors = { } -- table of colour name strings
-  },
+  -- },
 }
 
 -- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
