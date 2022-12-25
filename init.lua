aa78edf875cdf2fb2d98bb743c06073483be774a
chenweizi <chenweizi.chen@bytedance.com>
2022-12-25 15:53:47 +0800

    feat: 文件整理

--- init.lua
+++ init.lua
@@ -1,19 +1,20 @@
 ----------- 自动加载 after 里面的所有文件夹 ---------------
 require('base')
-require('keybindings')
-require('plugins')
-require('colorschema')
+
+-- require('keybindings')
+
+-- require('plugins')
+
+-- require('colorschema')
 
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
-
-
 if is_mac then
-  require('macos')
+  require('utils.macos')
 end
 if is_win then
-  require('windows')
+  require('utils.windows')
 end
 
 -- print(vim.fn.stdpath('data'))
